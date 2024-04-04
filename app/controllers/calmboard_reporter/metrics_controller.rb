870babbc866fb361bdebd1fe770c5a28dfe562b2
Greg Matthew Crossley <greg@neomindlabs.com>
2024-04-04 12:54:35 -0400

    Created the controller action

---
+++ app/controllers/calmboard_reporter/metrics_controller.rb
@@ -0,0 +1,13 @@
+require_dependency 'calmboard_reporter/encryption'
+
+module CalmboardReporter
+  class MetricsController < ActionController::Base
+    def show
+      metrics = Metrics::Base.check_all
+      encrypted_metrics = CalmboardReporter::Encryption.encrypt(metrics.to_json)
+      render json: {encrypted_metrics: encrypted_metrics}, status: :ok
+    rescue ArgumentError => e
+      render json: {error: e.message}, status: :unprocessable_entity
+    end
+  end
+end
